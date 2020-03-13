9756618a5de329422ce39cb3b95f5c3ae1ca04d2
Porson <porsonlee@gmail.com>
2020-03-13 01:46:00 -0700

    setting up mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://oddporson:tizilpX3@cluster0-vrlqz.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true});
+const cors = require('cors');
 const app = express();
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(cors());
+
+app.use('/api',require('./routes/index'))
+
 const port = process.env.PORT | 80
-app.get('/', (req, res) => {
-  res.send('hello world. welcome to backend')
-})
 
 
 
